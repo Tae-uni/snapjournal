2f823e2850a51e9628090ea91d0588b3c896b893
taekwon <universe134679@gmail.com>
2024-08-24 22:32:42 +0900

    :sparkles: add responsive Navbar on main page

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,16 +1,12 @@
+import { Navbar } from "@/components/layout/Navbar";
 import Image from "next/image";
 import Link from "next/link";
 
 export default function HomePage() {
   return (
     <main className="flex flex-col">
+    <Navbar />
       <h1>Welcome to the SnapJournal!</h1>
-      <div>
-        <Link className="underline ml-2" href="/signup" >Sign Up</Link>
-      </div>
-      <div>
-        <Link className="underline ml-2" href="/signin">Sign In</Link>
-      </div>
     </main>
   );
 }
--- frontend/src/components/layout/Navbar.tsx
+++ frontend/src/components/layout/Navbar.tsx
@@ -1,24 +1,70 @@
 "use client";
 
 import Link from "next/link";
+import { useState } from "react";
+import { MenuIcon, XIcon } from "lucide-react";
+import { Button } from "../ui/button";
 
 export const Navbar = () => {
-  return(
-    <nav className="bg-gray-800 text-white p-4 sm:p-6 md:flex md:justify-between md:items-center">
-      <div className="container mx-auto flex justify-between items-center">
-        <a href="" className="text-2xl font-bold">
+  const [isOpen, setIsOpen] = useState(false);
+
+  function getMenuClasses() {
+    let menuClasses = [];
+
+    if (isOpen) {
+      menuClasses = [
+        "flex",
+        "absolute",
+        "top-[60px]",
+        "bg-gray-800",
+        "w-full",
+        "p-6",
+        "left-0",
+        "gap-10",
+        "flex-col",
+        "item-center",
+      ];
+    } else {
+      menuClasses.push("hidden", "md:flex");
+    }
+
+    return menuClasses.join(" ");
+  }
+
+  return (
+    <nav className="bg-gray-800 flex justify-between text-white p-4 sm:p-6 md:flex md:justify-between md:items-center">
+      <div className="container justify-between mx-auto flex px-4 items-center">
+        <a href="" className="text-2xl font-bold mr-16">
           SnapJournal
         </a>
-        <div className="hidden md:flex">
-          <Link href="/" className="mx-2 hover:text-gray-300">
-            Home
+        <div className="flex ml-auto">
+          <div className={getMenuClasses()}>
+            <Link href="/" className="mx-5 hover:text-gray-300">
+              Home
+            </Link>
+            <Link href="/" className="mx-5 hover:text-gray-300">
+              How to use
+            </Link>
+            <Link href="/" className="mx-5 hover:text-gray-300">
+              Contact
+            </Link>
+          </div>
+        </div>
+        <Button variant="default" size="sm" className="mx-2">
+          <Link href="/signin">
+            Sign In
           </Link>
-          <Link href="/" className="mx-2 hover:text-gray-300">
-            How to use
-          </Link>
-          <Link href="/" className="mx-2 hover:text-gray-300">
-            Contact
-          </Link>
+        </Button>
+        <div className="md:hidden flex items-center">
+          <button onClick={() => {
+            setIsOpen(!isOpen);
+          }}>
+            {isOpen ? (
+              <XIcon className="h-6 w-6" />
+            ) : (
+              <MenuIcon className="h-6 w-6" />
+            )}
+          </button>
         </div>
       </div>
     </nav>
